148845217475c41562fa2347591d694e9b30556f
amsaif52 <am52saif@gmail.com>
2017-04-26 22:45:05 -0400

    Adding react native elements and getting data from an static source

---
+++ src/deck.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import {
+  View,
+  Text,
+  Animated
+} from 'react-native';
+
+
+
+class Deck extends Component{
+ renderCards(){
+   return this.props.data.map(item => {
+     return this.props.renderCard(item)
+   });
+ }
+ render(){
+   return(
+      <View>
+        {this.renderCards()}
+      </View>
+   );
+ }
+}
+
+export default Deck;
